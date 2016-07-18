9999386e2f7ac5e0de1474ad267bf2f7bf4bd3d3
karen818 <klc0818@outlook.com>
2016-07-18 09:08:35 -0500

    not all the features, but a start

---
+++ js/app.js
@@ -0,0 +1,29 @@
+(function(){
+    // var moment = require('moment');
+    var app = angular.module('redditApp', []);
+
+    app.controller("PostController", function($scope){
+        $scope.view = {};
+        $scope.newPost = {};
+        $scope.view.posts = [
+           {
+             postTitle: "I love Pickle",
+             author: "PokeStuff",
+             postUrl: "http://kingofwallpapers.com/images/images-169.jpg",
+             postTxt: "Derpy derpy do",
+             date: moment().subtract(2, 'days').calendar(),
+
+           }
+         ];
+
+        $scope.addPost = function(post){
+            post.date = moment().format('dddd, MMMM Do, YYYY');
+            $scope.view.posts.push(post);
+            $scope.newPost = {};
+            // $scope.postForm.$setUntouched();
+            console.log($scope.view.posts);
+        }
+
+    }); //end PostController
+
+})(); //end anonymous function wrapper
